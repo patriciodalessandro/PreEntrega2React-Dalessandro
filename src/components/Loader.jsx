fee8acf3ac2448a1ff0a3e1085f7e2796baa062e
“Patricio <dalessandropatricio@gmail.com>
2025-01-29 11:51:17 -0300

    final?

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,9 @@
 import { SpinnerCircular } from "spinners-react";
 
-
 export default function Loader() {
     return (
-        <SpinnerCircular size={50} thickness={100} speed={100} color="rgba(57, 83, 172, 1)" secondaryColor="rgba(0, 0, 0, 0.44)" />
+        <div className="flex items-center justify-center h-screen">
+            <SpinnerCircular size={50} thickness={100} speed={100} color="rgba(57, 83, 172, 1)" secondaryColor="rgba(0, 0, 0, 0.44)" />
+        </div>
     );
 }
